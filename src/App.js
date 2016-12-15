44d69a7d5078174ed7f52cac11187fa2dd7574b0
= <=>
2016-12-16 01:03:05 +0200

    set properties on react components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <h1>Hello World</h1>
+    let txt = this.props.txt;
+    return (
+      <div>
+        <h1>{txt}</h1>
+        <b>Bold</b>
+      </div>
+    )
   }
 }
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired,
+};
+
+App.defaultProps = {
+  txt: "this is the default text",
+};
 
 export default App
