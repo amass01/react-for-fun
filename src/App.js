5a0ecebcd9fe6d5660d2badc244cb0c220a6241d
= <=>
2016-12-16 01:12:24 +0200

    add component state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state text',
+      cat: 0,
+    }
+  }
+  update(e){
+    this.setState({txt:e.target.value})
+  }
   render(){
-    let txt = this.props.txt;
     return (
       <div>
-        <h1>{txt}</h1>
-        <b>Bold</b>
+        <input type="text"
+         onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt} - {this.state.cat}</h1>
       </div>
     )
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired,
-};
-
-App.defaultProps = {
-  txt: "this is the default text",
-};
 
 export default App
